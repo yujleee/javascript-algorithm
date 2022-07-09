60be37c39340c11599d57492d5d2ff28fe66adfd
이유정 <yujlee242@gmail.com>
2022-07-09 22:54:40 +0900

    📓   Q1. 선택정렬

---
+++ section7/Q1.js
@@ -0,0 +1,26 @@
+/**
+ * Q1. 선택정렬
+ *
+ * N개의 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+ * 정렬하는 방법은 선택정렬입니다.
+ */
+
+function solution(arr) {
+  let answer = arr;
+
+  //   answer.sort((a, b) => a - b);
+
+  // 선택정렬
+  for (let i = 0; i < arr.length; i++) {
+    let idx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[idx]) idx = j;
+    }
+    [arr[i], arr[idx]] = [arr[idx], arr[i]];
+  }
+
+  return answer;
+}
+
+let arr = [13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
